6b064a951a1878bfbd63f159bb1095aff82f871f
Ayush <ayushmohindru81@gmail.com>
2024-02-21 23:54:11 +0530

    Added node modules

--- db_conn.js
+++ db_conn.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"#MyPassword",
+    password:"#Godisthe1",
     database:"threads"
 });
 db.connect(function(err){
